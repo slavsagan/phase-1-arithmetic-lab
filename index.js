4e2c67bce10b0e9fe6669f3cad4bc1b11211407c
Slav <itiksolution@gmail.com>
2022-09-20 21:22:04 +1000

    Assignment complete

--- index.js
+++ index.js
@@ -1,2 +1,30 @@
+let num1 = 2
+let num2 = 31
+const multiply = (num1 * num2)
 
-// Write your code here
+console.log(multiply)
+
+// ========== OPTIONAL ===========
+// function count(num1, num2) {
+//   let multiply = num1 * num2
+//   return multiply
+// }
+// console.log(count(2, 31))
+
+
+let random = Number.isInteger()
+random = 32
+
+console.log (random)
+
+
+let num3 = 8, num4 = 2
+const mod = (num3 / num4)
+
+console.log (mod)
+
+
+let max = Math.max()
+max = (1, 4, 5, 15, 20)
+
+console.log (max)
